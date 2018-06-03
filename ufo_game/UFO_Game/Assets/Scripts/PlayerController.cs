db77d859641d979520bca91c741096a4a9b8b4de
Ricardo Mauro <ricardo.demauro@gmail.com>
2018-06-03 15:49:34 -0300

    adding ufo game

---
+++ ufo_game/UFO_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        _rigidbody2D.AddForce(movement * Speed, ForceMode2D.Force);
+    }
+}
